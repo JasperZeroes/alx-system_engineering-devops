d57714ad12bf796ed10fe5ea6fb46aa86a382ec6
JasperZeroes <jasperobed@gmail.com>
2024-05-14 21:33:46 +0100

    Delete 0x14-mysql directory

--- 0x14-mysql/create_and_grant_user.sql
+++
@@ -1,3 +0,0 @@
--- Create user and grant replication client permission
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
